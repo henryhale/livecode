4cb74da13833bf1425798b486035305374f34187
Henry Hale <92443116+henryhale@users.noreply.github.com>
2024-12-23 13:17:26 +0300

    chore: update logo

--- source/components/BaseLogo.vue
+++ source/components/BaseLogo.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex items-center space-x-2 px-2 group">
 		<img class="w-6 h-6" src="/logo.svg" />
-		<span class="hidden lg:inline-block text-blue-600">
+		<span class="hidden lg:inline-block text-blue-600 font-bold">
 			<b>LiveCode</b>
 		</span>
 	</div>
